91dfae187fc7c03edd202cfc0fa81727385e8522
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-02-07 17:23:01 +0530

    Time: 9 ms (20.37%), Space: 11.2 MB (29.40%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i] == target) {
-                return i;
+        int lo = 0, hi = nums.size()-1, mid;
+        while(lo <= hi) {
+            mid = (lo+hi)/2;
+            if(nums[mid] == target) {       // If key is present at middle point
+                return mid;
             }
+            if(nums[lo] <= nums[mid]) {
+                if(target <= nums[mid] and target >= nums[lo])
+                    hi = mid-1;
+                else
+                    lo = mid+1;
+            }
+            
+            else {
+                if(target >= nums[mid] and target <= nums[hi])
+                   lo = mid+1;
+                
+                else
+                    hi = mid-1;
+            }
+            
         }
         return -1;
     }
