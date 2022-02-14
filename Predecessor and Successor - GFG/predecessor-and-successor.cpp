d90fbac599452fbb115a889e737c031a8cc5b142
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-02-15 00:11:18 +0530

    Added solution - LeetHub

--- Predecessor and Successor - GFG/predecessor-and-successor.cpp
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -107,7 +107,10 @@ struct Node
 // This function finds predecessor and successor of key in BST.
 // It sets pre and suc as predecessor and successor respectively
 void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
-{
+{   
+    // If Node X has two children then its inorder predecessor is the maximum value in its 
+    // left subtree and its inorder successor the minimum value in its right subtree.
+    
     if(!root) {
         return;
     }
