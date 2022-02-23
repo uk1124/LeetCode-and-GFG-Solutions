d0ec73439d8e983ead53de72f024cedc7b809e1e
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-02-23 18:19:51 +0530

    Added solution - LeetHub

--- Get minimum element from stack - GFG/get-minimum-element-from-stack.cpp
+++ Get minimum element from stack - GFG/get-minimum-element-from-stack.cpp
@@ -34,28 +34,28 @@ class Solution{
            if(s.empty()) {
                return -1;
            }
-           int y = s.top();
-           s.pop();
-           if(y < minEle) {
-               minEle = 2*minEle - y;
-               return (y + minEle) / 2;
+           int y = s.top();     // take out the top element and store it
+           s.pop();             
+           if(y >= minEle) {    // if the top element is greater than min element simply return it
+               return y;
            }
            else {
-               return y;
+               minEle = 2*minEle - y;   // Else get the previous min element 
+               return (y + minEle) / 2; // use it to find the correct value of top element
            }
        }
        
        /*push element x into the stack*/
        void push(int x) {
-           if(s.empty()) {
+           if(s.empty()) {      // if stack is empty simply push the element
                s.push(x);
                minEle = x;
            }
-           else if(x > minEle) {
+           else if(x > minEle) { // if x is greater than or equal to current minimum simply push
                s.push(x);
            }
            else {
-               s.push(2*x - minEle);
+               s.push(2*x - minEle); // else modify the element and then push it. Remember we have to use this formulla to get back our original data while poping
                minEle = x;
            }
        }
