1acba9658adf94091e2104af32e4e6986f739dbb
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-02-25 14:16:35 +0530

    Time: 164 ms (58.85%), Space: 64.7 MB (61.34%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -11,44 +11,44 @@
  */
 class Solution {
 public:                     // RECURSIVE SOLUTION
-    // int sum = 0;
+    int sum = 0;
     int rangeSumBST(TreeNode* root, int low, int high) {
-//         if(!root) {
-//             return 0;
-//         }
+        if(!root) {
+            return 0;
+        }
         
-//         if(root->val >= low and root->val <= high) {
-//             sum += root->val;
-//         }
-//         if(root->val > low) {                  // Left child is possible candidate
-//             rangeSumBST(root->left, low, high);
-//         }
-//         if(root->val < high) {                 // Right child is possible candidate
-//             rangeSumBST(root->right, low, high);
-//         }
-//         return sum;
+        if(root->val >= low and root->val <= high) {
+            sum += root->val;
+        }
+        if(root->val > low) {                  // Left child is possible candidate
+            rangeSumBST(root->left, low, high);
+        }
+        if(root->val < high) {                 // Right child is possible candidate
+            rangeSumBST(root->right, low, high);
+        }
+        return sum;
         
         
                         // ITERATIVE SOLUTION
-        stack <TreeNode*> st;
-        st.push(root);
-        int sum = 0;
-        while (!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-            if (!node) { 
-                continue; 
-            }
-            if(node->val >= low and node->val <= high) {
-                sum += node->val;
-            }
-            if (node->val > low) {             // Left child is possible candidate
-                st.push(node->left); 
-            } 
-            if (node->val < high) { 
-                st.push(node->right);          // Right child is possible candidate
-            } 
-        }
-        return sum;
+        // stack <TreeNode*> st;
+        // st.push(root);
+        // int sum = 0;
+        // while (!st.empty()) {
+        //     TreeNode* node = st.top();
+        //     st.pop();
+        //     if (!node) { 
+        //         continue; 
+        //     }
+        //     if(node->val >= low and node->val <= high) {
+        //         sum += node->val;
+        //     }
+        //     if (node->val > low) {             // Left child is possible candidate
+        //         st.push(node->left); 
+        //     } 
+        //     if (node->val < high) { 
+        //         st.push(node->right);          // Right child is possible candidate
+        //     } 
+        // }
+        // return sum;
     }
 };
