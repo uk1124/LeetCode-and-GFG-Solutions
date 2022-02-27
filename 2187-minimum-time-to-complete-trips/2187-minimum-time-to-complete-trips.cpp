f31fb8a7c32daff7c8f4a0c9f036dc201f771305
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-02-27 10:02:30 +0530

    Time: 960 ms (5.26%), Space: 88.9 MB (31.58%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -10,17 +10,17 @@ public:             // APPROACH: Using Binary Search
             long long mid = (l + h) / 2;
             long long count = 0;
             
-            for(int i=0; i<time.size(); i++) {
+            for(int i=0; i<time.size(); i++) { // For a given amount of time, calculate max no. of trips
                 if(time[i] > mid) {
                     break;
                 }
                 count += (mid/time[i]);
             }
-            if(count >= totalTrips) {
+            if(count >= totalTrips) {   // If count is greater than totalTrips, search in [1, mid-1]
                 minTime = mid;
                 h = mid-1;
             }
-            else {
+            else {                      // Else search in [mid+1, h]
                 l = mid+1;
             }
         }
