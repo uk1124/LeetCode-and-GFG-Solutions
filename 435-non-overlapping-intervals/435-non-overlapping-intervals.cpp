ec10ed450d5bed1d08948168cc358b6ccd578bbe
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-03-07 23:46:47 +0530

    Time: 586 ms (61.67%), Space: 89.9 MB (20.96%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+    // APPROACH: Think about all the possible cases of overlapping remove the largest inteval(s)
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         int count = 0;
         int left = 0, right = 1;
         
-       while(right < intervals.size())                           // Unless all intervals are compared
-        {
-            if(intervals[left][1] <= intervals[right][0]) {      // Non-overlapping case 
+       while(right < intervals.size()) {                          // Unless all intervals are compared
+            if(intervals[left][1] <= intervals[right][0]) {       // Non-overlapping case 
                 left = right;
                 right++;
             }
-            else if(intervals[left][1] <= intervals[right][1]) { // Overlapping - Remove right interval
-                count++;                                         //   Delete right
+            else if(intervals[left][1] <= intervals[right][1]) {  // Overlapping - Remove right interval
+                count++;                                          //   Delete right
                 right++;
             }
-            else if(intervals[left][1] > intervals[right][1]) {  // Overlapping - Remove left interval
+            else if(intervals[left][1] > intervals[right][1]) {   // Overlapping - Remove left interval
                 count++;
                 left = right;
                 right++;
