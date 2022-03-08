e386bbc21805aa3a4fcc880617b1dac1b5b00be5
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-03-08 22:12:51 +0530

    Time: 72 ms (88.10%), Space: 38 MB (92.81%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // APPROACH:  Start from end and search for valid (s2,s3) pairs, we just need to remember the largest valid s3 value.
+    bool find132pattern(vector<int>& nums) {
+        stack <int> s;
+        int second =  INT_MIN;
+        
+        for (int i=nums.size()-1; i>=0; i--) {
+            if (nums [i] < second) {
+                return true;
+            }
+            while (!s.empty() and nums[i] > s.top()) {
+                second = s.top ();
+                s.pop();
+            }   
+            s.push (nums [i]);
+        }
+        return false;
+    }
+};
