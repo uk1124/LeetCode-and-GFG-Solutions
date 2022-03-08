aa99b6206fa53031bb73949326ec638f74664a8f
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-03-08 11:18:25 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -92,6 +92,11 @@ struct Node
 }; */
 
 class Solution {
+    /* APPROACH: The diameter of a tree T is the largest of the following quantities:
+    -> the diameter of T's left subtree.
+    -> the diameter of T's right subtree.
+    -> the longest path between leaves that goes through the root of T (this can be computed from the heights of the subtrees of T)
+    */
   public:
     int Diameter(Node* root,int &height) {
         if(!root) {
