bdb815527f6907bb820e2fc7a03a03094184f185
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-03-15 17:57:05 +0530

    Time: 28 ms (63.77%), Space: 11.6 MB (54.07%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack <int> st;
+        string ans = "";
+        
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            }
+            else if(s[i] == ')') {
+                if(st.empty()) {
+                    s[i] = '#';
+                    
+                }
+                else {
+                    st.pop();
+                }
+            }
+        }
+        while(!st.empty()) {
+            s[st.top()] = '#';
+            st.pop();
+        }
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] != '#') {
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
