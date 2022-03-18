bc01383881ce48d66852299a5ee3f8afb0d48385
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-03-18 08:58:08 +0530

    Time: 7 ms (30.82%), Space: 6.8 MB (52.77%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -13,7 +13,7 @@ public:
             if(visited[ch]) {
                 continue;
             }
-            while(ch < ans.back() and counter[ans.back()] > 0) {
+            while(ch < ans.back() and counter[ans.back()] > 0) {    // ch < result.back() is for making ans as lexicographically small as possible, counter[ans.back()] > 0 ensures that there is at least one such element(ans.back()) left after pop_back(), to be append to ans later (so that each letter would appear at least once)
                 visited[ans.back()] = false;
                 ans.pop_back();
             }
