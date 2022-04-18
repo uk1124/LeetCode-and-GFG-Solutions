8ade8f93cc4736b6f78494e5d2bae7a7c18773a1
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-04-18 23:24:10 +0530

    Added solution - LeetHub

--- K-Ary Tree - GFG/kary-tree.cpp
+++ K-Ary Tree - GFG/kary-tree.cpp
@@ -9,22 +9,22 @@ using namespace std;
 
 class Solution {
   public:
-    long long karyTree(int k, int m) {
-       long long res = 1, k1 = k, mod = 1e9+7;
-    while(m) {
-        if(m&1) {
-            res *= k1;
-            res %= mod;
-            m--;
-        } 
-        else {
-            k1 = k1*k1;
-            k1 %= mod;
-            m /= 2;
-        }
-    }
-    return res;
-    }
+  long long karyTree(int k, int m) {
+      long long res = 1, k1 = k, mod = 1e9+7;
+      while(m) {
+          if(m&1) {
+              res *= k1;
+              res %= mod;
+              m--;
+          }
+          else {
+              k1 = k1*k1;
+              k1 %= mod;
+              m /= 2;
+          }
+      }
+      return res;
+  }
 };
 
 // { Driver Code Starts.
