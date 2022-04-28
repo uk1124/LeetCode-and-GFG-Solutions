9d75a226f8cd06e344956dbf97891038cfc1b161
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-04-28 22:33:07 +0530

    Time: 53 ms (93.09%), Space: 21.8 MB (34.64%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int dfs_check(vector<vector<int>>& grid, int i, int j, int rows, int columns) {
+        if(i < 0 or j < 0 or i >= rows or j >= columns or grid[i][j] == 0) {
+            return 0;
+        }
+        
+        grid[i][j] = 0;
+        
+        return (1 + dfs_check(grid,i-1,j,rows,columns) + dfs_check(grid,i+1,j,rows,columns) + dfs_check(grid,i,j-1,rows,columns) + dfs_check(grid,i,j+1,rows,columns));
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int columns = grid[0].size();
+        int total_ones = 0, closed_ones = 0;
+        
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<columns; j++) {
+                if(grid[i][j] == 1) {
+                    total_ones++;
+                }
+            }
+        }
+        
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<columns; j++) {
+                if((i == 0 or j == 0 or i == rows-1 or j == columns-1) and (grid[i][j] == 1)) {
+                    closed_ones += dfs_check(grid, i, j, rows, columns);
+                }
+            }
+        }
+        return (total_ones - closed_ones);
+    }
+};
