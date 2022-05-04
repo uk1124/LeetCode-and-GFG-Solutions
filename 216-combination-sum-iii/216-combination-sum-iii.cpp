dda304dfd653993cfa969627c7c3aaa39783298e
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-05-04 10:28:11 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.21%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void findCombinations(int start, int k, int target, vector<int>&ds, vector<int>&nums, vector<vector<int>>&ans) {
+         if(k == 0 and target == 0) {
+             ans.push_back(ds);
+             return;
+         }
+        
+        for(int i=start; i<=10-k and i<=target; i++) {
+            ds.push_back(i);
+            findCombinations(i+1, k-1, target-i, ds, nums, ans);
+            ds.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> nums = {1,2,3,4,5,6,7,8,9};
+        vector<int> ds;
+        vector<vector<int>> ans;
+        findCombinations(1, k, n, ds, nums, ans);
+        return ans;
+    }
+};
