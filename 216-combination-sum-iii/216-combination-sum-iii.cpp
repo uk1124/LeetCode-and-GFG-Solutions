4b09e5f2fc65c37e505fd3b9fbb17715cd816add
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-05-04 10:36:32 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (94.19%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
+    //APPROACH: Using Backtracking
     void findCombinations(int start, int k, int target, vector<int>&ds, vector<vector<int>>&ans) {
-         if(k == 0 and target == 0) {
+         if(k == 0 and target == 0) {       // Got the answer
              ans.push_back(ds);
              return;
          }
-        // if(target < 0 or ds.size() > k) {
-        //     return;
-        // }
-      
+        
         for(int i=start; i<=10-k and i<=target; i++) {
             ds.push_back(i);
             findCombinations(i+1, k-1, target-i, ds, ans);
             ds.pop_back();
         }
-        // findCombinations(i+1,k,target-nums[i],ds,nums);
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        // vector<int> nums = {1,2,3,4,5,6,7,8,9};
         vector<int> ds;
         vector<vector<int>> ans;
         findCombinations(1, k, n, ds, ans);
