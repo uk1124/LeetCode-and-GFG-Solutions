113872c0a13ede5de7c5ddcab0c1fbecf8ef7304
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-05-12 00:08:15 +0530

    Time: 30 ms (9.50%), Space: 15.7 MB (44.41%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    //APPROACH: Use Binary Search.
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int lo = 0;
+        int hi = numbers.size()-1;
+        
+        while(lo < hi) {
+            if(numbers[lo] + numbers[hi] == target) {
+                return {lo+1, hi+1};;
+            }
+            else if(numbers[lo] + numbers[hi] > target) {
+                hi--;
+            }
+            else {
+                lo++;
+            }
+        }
+        return {};
+    }
+};
