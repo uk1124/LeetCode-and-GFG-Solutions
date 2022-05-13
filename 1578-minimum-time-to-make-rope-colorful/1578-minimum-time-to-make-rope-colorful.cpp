0bf92d825c2c43692fb2b6ca3c2440dc7945f7ec
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-05-13 23:41:25 +0530

    Time: 153 ms (88.08%), Space: 95.4 MB (87.84%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,13 +1,14 @@
 class Solution {
-public:     // APPROACH:  If we have repeated characters, we need to remove all of them except the 'most expensive' character.
+public:
+    //APPROACH: If we have repeated characters, we need to remove all of them except the 'most expensive' character.
     int minCost(string colors, vector<int>& neededTime) {
         int max_cost = 0, ans = 0;
         
         for(int i = 0; i<colors.size(); i++) {
-             if(i>0 and colors[i] != colors[i-1]){ // Current character is diff than previous one, so subtract max_cost and set it to zero.
+             if(i > 0 and colors[i] != colors[i-1]){ //Current character is diff than previous one, so subtract max_cost and set it to zero.
                 ans -= max_cost;
                 max_cost = 0;
-            }                   // Repeating characters, so add time to remove all of them except the most expensive one.
+            }                   //Repeating characters, so add time to remove all of them except the most expensive one.
             ans += neededTime[i];       
             max_cost = max(max_cost, neededTime[i]);
         }
