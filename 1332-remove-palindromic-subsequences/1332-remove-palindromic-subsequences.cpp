aaaa60c7b7350d4559e9c451e859c3ec2401723a
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-06-08 23:55:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+    /*Important point to note: Subarray need to be consecutive。
+      Subsequence don't have to be consecutive. */
+
     int removePalindromeSub(string s) {
+        if(s == "") {
+            return 0;
+        }
         string t = s;
         reverse(s.begin(), s.end());
         if(t == s) {
