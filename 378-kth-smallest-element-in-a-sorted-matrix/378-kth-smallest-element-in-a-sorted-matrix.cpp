657bd402dafe81e74860863204ee4daa60ec5736
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-06-14 21:27:56 +0530

    Time: 338 ms (5.01%), Space: 20.6 MB (5.03%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    /*APPROCH: Using Min heap
+    Since each of the rows in matrix are already sorted, we can understand the problem as finding the kth smallest
+    element from amongst M sorted rows.
+    We start the pointers to point to the beginning of each rows, then we iterate k times, for each time ith, the
+    top of the minHeap is the ith smallest element in the matrix. We pop the top from the minHeap then add the next
+    element which has the same row with that top to the minHeap.*/
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        int m = matrix.size(), n = matrix[0].size(), ans;
+        
+        for(int i=0; i<min(m, n); i++) {
+            pq.push({matrix[i][0], i, 0});
+        }
+        
+        for(int i = 1; i <= k; i++) {
+            auto top = pq.top(); 
+            pq.pop();
+            int r = top[1];
+            int c = top[2];
+            ans = top[0];
+            if (c+1 < n) {
+                pq.push({matrix[r][c+1], r, c+1});
+            }
+        }
+        return ans;
+    }
+    // TC: O(K * logK)  SC: O(K)
+};
