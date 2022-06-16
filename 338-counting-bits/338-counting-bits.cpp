a1a80b1a7c272881264549a3eb63e4942f954a93
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-06-16 20:35:56 +0530

    Time: 13 ms (20.73%), Space: 7.8 MB (87.96%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
+    /*APPROACH: Using DP (Bottom Up)
+    Notable point: In even number the count of '1's is always the same as its half number, on the other hand, it's
+    plus one to the odds' half number. */
     vector<int> countBits(int n) {
-        vector<int> ans(n+1, 0);
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        
         for(int i=1; i<=n; i++) {
-            ans[i] = __builtin_popcount(i);
+            dp[i] = dp[i/2] + (i % 2);
         }
-        return ans;
+        return dp;
     }
 };
