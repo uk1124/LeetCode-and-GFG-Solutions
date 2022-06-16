e22039b8edaa8ef30b13feac1cf88241e30783b7
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-06-16 21:08:55 +0530

    Time: 19 ms (48.03%), Space: 9.5 MB (66.38%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    //APPROACH: DFS
     int fill(vector<vector<int>>& g, int i, int j) {
-    if (i < 0 or j < 0 or i >= g.size() or j >= g[i].size() or g[i][j] == 1) {
+    if (i<0 or j<0 or i>=g.size() or j>=g[i].size() or g[i][j]==1) {
         return 0;
     }
-    return (g[i][j] = 1) + fill(g, i + 1, j) + fill(g, i, j + 1) + fill(g, i - 1, j) + fill(g, i, j - 1);
+    return (g[i][j] = 1) + fill(g, i+1, j) + fill(g, i, j+1) + fill(g, i-1, j) + fill(g, i, j-1);
 }
     
     int closedIsland(vector<vector<int>>& g) {
-        int res = 0;
+        int ans = 0;
         for (int i = 0; i < g.size(); i++) {
             for (int j = 0; j < g[i].size(); j++) {
-                if (i * j == 0 || i == g.size() - 1 || j == g[i].size() - 1) {
+                if (i*j == 0 or i == g.size()-1 or j == g[i].size()-1) {
                     fill(g, i, j);
                 }
             }
         }
         
-        for (int i = 0; i < g.size(); i++) {
-            for (int j = 0; j < g[i].size(); j++) {
-                res += fill(g, i, j) > 0;
+        for (int i=0; i<g.size(); i++) {
+            for (int j=0; j<g[i].size(); j++) {
+                ans += fill(g, i, j) > 0;
             }
         }
-        
-        return res;
+        return ans;
     }
 };
