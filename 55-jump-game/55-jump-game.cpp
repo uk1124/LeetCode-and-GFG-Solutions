b2f3b0f43f4adc5d9e005827d8d00a0ca9283a01
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-06-24 12:29:19 +0530

    Time: 1030 ms (8.37%), Space: 48.5 MB (22.17%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    /*APPROACH: Greedy
+    Looking from the start and selecting the locally optimum in the hope of reaching global optimum */
+    // bool canJump(vector<int>& nums) {
+    //     int currIdx = 0;
+    //     for(int i = 0; i < nums.size(); i++) {
+    //         if(currIdx < i) {
+    //             return false;
+    //         }
+    //         currIdx = max(i + nums[i], currIdx);
+    //     }
+    //     return true;
+    // }
+    
+    /*APPROACH: Using DP (Bottom Up)
+    Looking from the end and at each point ahead checking the best possible way to reach the end */
     bool canJump(vector<int>& nums) {
-        int currIdx = 0;
-        for(int i=0; currIdx < nums.size() and currIdx <= i; currIdx++) {
-            i = max(currIdx + nums[currIdx], i);
-        }
+        int n = nums.size();
+        vector<bool> jump(n, false);
+        jump[n-1] = true;
         
-        if(currIdx == nums.size()) {
-            return true;
+        for(int i=n-2; i>=0; i--) {
+            for(int j=0; j <= nums[i] and i+j < n; j++) {
+                if(jump[i+j])  {
+                    jump[i] = true; 
+                    break;
+                }
+            }
         }
-        return false;
+        return jump[0];
     }
 };
