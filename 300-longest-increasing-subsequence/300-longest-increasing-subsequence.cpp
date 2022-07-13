1ab3795b74ef6becb69a5a44ce81d444b40a964d
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-07-13 08:19:50 +0530

    Time: 599 ms (21.19%), Space: 10.6 MB (24.23%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int dp[2510];                      // dp[i] is the longest increase subsequence which ends at nums[i]
-    int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        int n = nums.size();
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            ans = max(ans, LIS(i, nums));
-        }
-        return ans;
-    }
-    
-    int LIS(int i, vector<int> &a) {
+    //APPROACH: Using DP
+    int LIS(int i, vector<int> &a, vector<int> &dp) {
         if(dp[i] != -1) {
             return dp[i];
         }
         int ans = 1;
         for(int j=0; j<i; j++) {
             if(a[i] > a[j]) {
-                ans = max(ans, LIS(j, a)+1);
+                ans = max(ans, LIS(j, a, dp)+1);
             }
         }
         return dp[i] = ans;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            ans = max(ans, LIS(i, nums, dp));
+        }
+        return ans;
     }
 };
