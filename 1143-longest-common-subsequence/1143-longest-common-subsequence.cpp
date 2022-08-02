88602e891c576006a857cb376e5765a61f3a48cd
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-02 15:33:40 +0530

    Time: 59 ms (16.90%), Space: 13.3 MB (11.09%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //APPROACH: Using DP (Top Down)
     int lcs(int i, int j, string &text1, string &text2, vector<vector<int>> &dp) {
         if(i < 0 or j < 0) {
             return 0;
@@ -9,9 +10,9 @@ public:
             return dp[i][j];
         }
         
-        int ans1 = lcs(i-1, j, text1, text2, dp);
+        int ans1 = lcs(i-1, j, text1, text2, dp);       //Removing character from 1st string keeping 2nd string same
         
-        int ans2 = lcs(i, j-1, text1, text2, dp);
+        int ans2 = lcs(i, j-1, text1, text2, dp);       //Removing character from 2nd string keeping 1st string same
         
         int ans3 = lcs(i-1, j-1, text1, text2, dp) + (text1[i]==text2[j]);  //Check if both the characters are equal
         
