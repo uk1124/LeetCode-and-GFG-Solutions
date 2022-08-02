d726e70e33f6738afe6d267bc0b75bacf7e277d7
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-02 14:58:55 +0530

    Time: 521 ms (22.43%), Space: 76.3 MB (13.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
+    //APPROACH: Using DP (Top Down)
     bool solveMem(int indx, int sum, vector<int> &A, vector<vector<int>> &dp) {
         if(sum == 0) {
             return true;
         }
-        
         if(indx < 0) {
             return false;
         }
@@ -13,10 +13,10 @@ public:
             return dp[indx][sum];
         }
         
-        bool isPossible = solveMem(indx-1, sum, A, dp);
+        bool isPossible = solveMem(indx-1, sum, A, dp);             //Not Choose
         
         if(sum - A[indx] >= 0) {
-            isPossible |= solveMem(indx-1, sum - A[indx], A, dp);
+            isPossible |= solveMem(indx-1, sum - A[indx], A, dp);   //Choose
         }
         return dp[indx][sum] = isPossible;
     }
