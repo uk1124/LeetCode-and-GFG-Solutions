429fc1d020fc44eaf4b753265dd9d918f90706e1
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-05 09:44:09 +0530

    Time: 3 ms (86.89%), Space: 13.7 MB (48.15%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    void solve(vector<string> &ans, string s, int n, int m) {
-        if(n == 0 and m == 0) {
+    /*APPROACH: Using Recursion.
+    'left', represents how many left parentheses remains; 'right' represents how many right parentheses remains. The
+    remaining right parentheses should be larger than left ones. */
+    void solve(vector<string> &ans, string s, int left, int right) {
+        if(left == 0 and right == 0) {
             ans.push_back(s);
             return;
         }
-        if(m > 0) {
-            solve(ans, s + ')', n, m-1);
+        if(left > 0) {
+            solve(ans, s + "(", left-1, right);
         }
-        if(n > 0) {
-            solve(ans, s + '(', n-1, m+1);
+        if(right > left) {
+            solve(ans, s + ")", left, right-1);
         }
     }
     
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        solve(ans, "", n, 0);
+        solve(ans, "", n, n);
         return ans;
     }
 };
