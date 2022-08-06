5f03b3a5cd8b3aad167b4c1e64de5167b9b4f10e
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-06 18:49:59 +0530

    Time: 8 ms (78.18%), Space: 14.1 MB (8.09%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
+    /*APPROACH: Using DP (Bottom Up)
+     Use dpMax and dpMin arrays where dpMax[i] denotes maximum subarray product ending at i and dpMin[i] denotes
+     minimum subarray product ending at i.
+     At each index i, we will update dpMin[i] as minimum of A[i] (denotes forming new subarray by choosing current
+     element) and min(A[i] * dpMin[i-1], A[i] * dpMax[i-1]) (denotes expanding previous subarray product by 
+     current element). We do it similarly for dpMax[i] as well. Finally, the maximum product subarray will be the
+     maximum value in dpMax array. */
     int maxProduct(vector<int>& nums) {
         vector<int> dpMin(nums), dpMax(nums);
         int ans = nums[0];
+        
         for(int i = 1; i < size(nums); i++) {
             if(nums[i] >= 0) {
                 dpMin[i] = min(nums[i], nums[i] * dpMin[i-1]);
@@ -12,10 +20,6 @@ public:
                 dpMin[i] = min(nums[i], nums[i] * dpMax[i-1]);
                 dpMax[i] = max(nums[i], nums[i] * dpMin[i-1]);
             }
-            
-            // dpMin[i] = min(nums[i], nums[i] * (nums[i] >= 0 ? dpMin[i-1] : dpMax[i-1]));
-            // dpMax[i] = max(nums[i], nums[i] * (nums[i] >= 0 ? dpMax[i-1] : dpMin[i-1]));
-            
             ans = max(ans, dpMax[i]);
         }
         return ans;
