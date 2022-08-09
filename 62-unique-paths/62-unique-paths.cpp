8efef5008dc8b37d83a9edb1c46589fa49d42718
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-09 10:05:51 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
+    /*APPROACH: Using DP. 
+    We consider both of the moves (Down and Right).
+    If the series of moves leads to a cell outside the grid's boundary, we can return 0 denoting no valid path was
+    found.
+    If the series of moves leads us to the target cell (m-1, n-1), we return 1 denoting we found a valid unique path
+    from start to end.
+    */
     int solve(int i, int j, vector<vector<int>> &dp) {
-        if(i < 0 or j < 0) {
+        int m = dp.size() - 1;
+        int n = dp[0].size() - 1;
+        
+        if(i > m or j > n) {
             return 0;
         }
-        if(i == 0 and j == 0) {
+        if(i == m-1 and j == n-1) {                 // We got a possible path
             return 1;
         }
         if(dp[i][j] != -1) {
             return dp[i][j];
         }
         else {
-            return dp[i][j] = solve(i, j-1, dp) + solve(i-1, j, dp);
+            return dp[i][j] = solve(i, j+1, dp) + solve(i+1, j, dp);
         }
     }
     
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
-        return solve(m-1, n-1, dp);
+        return solve(0, 0, dp);
     }
 };
