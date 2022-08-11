37bf0ee838bcd307f73ca292ee219f8203d0b37f
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-11 11:19:42 +0530

    Time: 86 ms (48.15%), Space: 27.2 MB (7.17%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     /*APPROACH: USing DP (Top Down)
     Trick: Just find the size of Longest Palindromic Subsequence and subtract it from the original string size*/
-    int solveMem(int i, int j, string &s, vector<vector<int>> &dp) {    
+    int LPS(int i, int j, string &s, vector<vector<int>> &dp) {    
         if(i >= j) {
             return (i==j);
         }
@@ -11,10 +11,10 @@ public:
             return dp[i][j];
         }
         
-        int ans = solveMem(i+1, j, s, dp);
-        ans = max(ans, solveMem(i, j-1, s, dp));
+        int ans = LPS(i+1, j, s, dp);
+        ans = max(ans, LPS(i, j-1, s, dp));
         if(s[i] == s[j]) {
-            ans = max(ans, solveMem(i+1, j-1, s, dp) + 2);
+            ans = max(ans, LPS(i+1, j-1, s, dp) + 2);
         }
         return dp[i][j] = ans;
     }
@@ -23,7 +23,7 @@ public:
         int n = s.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         
-        int sizeLPS =  solveMem(0, n-1, s, dp);
+        int sizeLPS =  LPS(0, n-1, s, dp);
         
         return n - sizeLPS;
     }
