21c48df5c5eb16f8f56c8e78ab1434b7d7aed3e6
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-24 10:56:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (11.41%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+    /*APPROACH: Using DP (Top Down)
+    In general (Catalan Numbers):
+    f[n] = f[0]*f[n-1] + f[1]*f[n-2] + ... + f[n-2]*f[1] + f[n-1]*f[0].
+    */
     int solveMem(int n, vector<int> &dp) {
-        if(n == 0 or n == 1) {
+        if(n == 0 or n == 1) {              //Base Case
             return 1;
         }
         int curr = n;
