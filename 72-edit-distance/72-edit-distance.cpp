1623ca4e856f340fa6ba2a6acbaa7b38db50234f
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2022-08-30 11:28:50 +0530

    Time: 107 ms (18.13%), Space: 7.4 MB (76.21%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    /*APPROACH: Using DP (Top Down).
+    i and j represent two characters from strings word1 and word2 respectively. There are only two options that make
+    sense: either the characters represented by i and j match or they don’t.
+    If the characters match in both strings  return 0 + f(i-1,j-1)
+    If the characters don't match in both strings 
+        - return 1 + f(i-1,j)     --> Insertion of character.
+        - return 1 + f(i,j-1)     --> Deletion of character.
+        - return 1 + f(i-1,j-1)   --> Replacement of character.
+    */
     int dp[505][505];
     int solve(int i, int j, string &word1, string &word2) {
         if(i < 0) {
@@ -13,11 +22,11 @@ public:
             return dp[i][j];
         }
         
-        if(word1[i] == word2[j]) {
+        if(word1[i] == word2[j]) {                  //If the characters match
             dp[i][j] = solve(i-1, j-1, word1, word2);
             return dp[i][j];
         }
-        else {
+        else {                                      //If the characters don't match
             dp[i][j] = 1 + min(solve(i-1,j-1,word1,word2), min(solve(i,j-1,word1,word2), solve(i-1,j,word1,word2)));
             return dp[i][j];
         }
