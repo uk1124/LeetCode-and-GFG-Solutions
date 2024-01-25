56fe0a8056ba943db62ebc1d9109da20879c0047
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-01-25 09:34:45 +0530

    Time: 5 ms (58.71%), Space: 11.3 MB (11%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    string simplifyPath(std::string path) {
-        stack<string> st;
-        vector<string> components;
+    string simplifyPath(string path) {
+        stack<string> st;           // Stack to keep track of valid components
+        vector<string> components;  // Vector to store individual components of the path
 
-        for (int i = 0; i < path.size(); ++i) {
+        for(int i = 0; i < path.size(); ++i) {
             if (path[i] == '/') {
-                continue;  // Ignore consecutive slashes
+                continue;       // Ignore consecutive slashes
             }
-
             string temp = "";
-            while (i < path.size() and path[i] != '/') {
+            while(i < path.size() and path[i] != '/') {
                 temp += path[i++];
             }
-            
-            if (temp == "..") {
+            // Process different cases
+            if(temp == "..") {
                 if (!st.empty()) {
-                    st.pop();
+                    st.pop();   // Move up one level if '..' encountered
                 }
             } 
             else if (temp != ".") {
-                st.push(temp);
+                st.push(temp);  // Push valid components onto the stack
             }
         }
 
-        while (!st.empty()) {
+        // Reconstruct the simplified path from the stack
+        while(!st.empty()) {
             components.insert(components.begin(), st.top());
             st.pop();
         }
 
         string ans = "/";
-        for (const auto& comp : components) {
-            ans += comp + "/";
+        for(const auto& comp : components) {
+            ans += comp + "/";  // Add '/' between components
         }
 
-        if (ans.size() > 1) {
-            ans.pop_back();  // Remove the trailing '/'
+        if(ans.size() > 1) {
+            ans.pop_back();     // Remove the trailing '/'
         }
 
         return ans;
     }
 };
+
