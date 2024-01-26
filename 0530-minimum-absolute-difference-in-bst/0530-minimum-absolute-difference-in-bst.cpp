3b350e7bd7b54e5977995bed82855a727a72b7b3
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-01-26 11:13:55 +0530

    Time: 15 ms (53.78%), Space: 23.5 MB (99.74%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+/*APPROACH: Perform an in-order traversal of the BST and calculate the min absolute difference between consecutive node values. The min difference is updated during traversal, resulting in the overall min absolute difference between any two nodes in the tree.
+*/
+public:
+    // Variables to store the min absolute difference and previous node's value during traversal
+    int ans = INT_MAX;
+    int prev = INT_MAX;
+
+    // DFS function to traverse the BST
+    void dfs(TreeNode *root) {
+        // Traverse the left subtree if it exists
+        if (root->left) {
+            dfs(root->left);
+        }
+
+        // Calculate the absolute diff between the current node's value & the previous node's value
+        ans = min(ans, abs(root->val - prev));
+
+        // Update the previous node's value
+        prev = root->val;
+
+        // Traverse the right subtree if it exists
+        if (root->right) {
+            dfs(root->right);
+        }
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
