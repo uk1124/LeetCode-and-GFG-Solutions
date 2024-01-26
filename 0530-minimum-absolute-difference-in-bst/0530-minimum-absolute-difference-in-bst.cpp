1f8f4a5c9465e9e8d46765572ca87326abb78620
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-01-26 11:14:46 +0530

    Time: 12 ms (69.44%), Space: 23.6 MB (98.28%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -12,11 +12,11 @@
 class Solution {
 /*APPROACH: Perform an in-order traversal of the BST and calculate the min absolute difference between consecutive node values. The min difference is updated during traversal, resulting in the overall min absolute difference between any two nodes in the tree.
 */
-public:
+private:
     // Variables to store the min absolute difference and previous node's value during traversal
     int ans = INT_MAX;
     int prev = INT_MAX;
-
+public:
     // DFS function to traverse the BST
     void dfs(TreeNode *root) {
         // Traverse the left subtree if it exists
@@ -26,7 +26,6 @@ public:
 
         // Calculate the absolute diff between the current node's value & the previous node's value
         ans = min(ans, abs(root->val - prev));
-
         // Update the previous node's value
         prev = root->val;
 
