5f808950b4868c7daecdaabd2f7f593bc5438cc6
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-02-01 09:23:56 +0530

    Time: 129 ms (99.04%), Space: 119.6 MB (39.25%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        // Sort the input array
         sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
 
         // Check each window of size 3
         for (int i = 0; i < nums.size(); i += 3) {
@@ -10,17 +13,10 @@ public:
                 // If the condition is not satisfied, return an empty vector
                 return {};
             }
+            // Add the current window to the result
+            ans.push_back({nums[i], nums[i+1], nums[i + 2]});
         }
 
-        // If all windows are valid, construct and return the answer
-        vector<vector<int>> ans;
-        for (int i = 0; i < nums.size(); i += 3) {
-            vector<int> temp;
-            for (int j = i; j < i + 3; j++) {
-                temp.push_back(nums[j]);
-            }
-            ans.push_back(temp);
-        }
         return ans;
     }
 };
