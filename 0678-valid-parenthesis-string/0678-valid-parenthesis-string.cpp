061ead25f3e789022be18490d5f408aa8dbf862b
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-07 17:20:07 +0530

    Time: 0 ms (100%), Space: 7.6 MB (27.25%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> leftParentheses; // Stack to store indices of '('
+        stack<int> stars;           // Stack to store indices of '*'
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                leftParentheses.push(i);        // Store index of '('
+            } 
+            else if (s[i] == '*') {
+                stars.push(i);      // Store index of '*'
+            } 
+            else {                  // i.e s[i] == ')'
+                if (!leftParentheses.empty()) {
+                    leftParentheses.pop();      // Match '(' with ')'
+                } 
+                else if (!stars.empty()) {
+                    stars.pop();    // Use '*' as '('
+                } 
+                else {
+                    return false;   // No matching '(' or '*' found
+                }
+            }
+        }
+
+        // Match remaining '*' with ')'
+        while (!leftParentheses.empty() and !stars.empty()) {
+            if (leftParentheses.top() < stars.top()) {
+                leftParentheses.pop();
+                stars.pop();
+            } 
+            else {
+                break;
+            }
+        }
+
+        // If there are no remaining '(' to match with '*', stack of '(' should be empty
+        return leftParentheses.empty();
+    }
+};
