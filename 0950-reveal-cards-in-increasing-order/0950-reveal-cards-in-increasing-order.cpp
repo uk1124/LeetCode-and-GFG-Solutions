56f23031c6952a9858e702188a6ab6f20eff39b0
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-10 12:58:32 +0530

    Time: 0 ms (100%), Space: 10.8 MB (67.85%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -3,10 +3,10 @@ public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         sort(deck.begin(), deck.end());
         int n = deck.size();
-        vector<int>  ans;
+        vector<int>  ans(n);
         queue<int> q;
         for(int i = 0; i < n; i++) {
-            q.push(i++);
+            q.push(i);
         }
         for(int i=0; i<n; i++) {
             ans[q.front()] = deck[i];
@@ -14,6 +14,6 @@ public:
             q.push(q.front());
             q.pop();
         }
-        
+        return ans;
     }
 };
