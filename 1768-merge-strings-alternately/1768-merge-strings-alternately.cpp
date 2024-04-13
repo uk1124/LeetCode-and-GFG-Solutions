f974cdcdbba434950d11b1988601c1cff138d1df
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-13 11:39:21 +0530

    Time: 3 ms (47.49%), Space: 7.5 MB (79.62%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans = "";
-        int maxLength = max(word1.size(), word2.size()); 
+        string ans = ""; 
+        int maxLength = max(word1.size(), word2.size());
 
-        for(int i = 0; i < maxLength; i++) {
-            if(i < word1.size()) {
-                ans.push_back(word1[i]);
+        // Iterate through the indices up to the maximum length
+        for(int i = 0; i < maxLength; i++) { 
+            if(i < word1.size()) {       // If there are more characters in word1
+                ans.push_back(word1[i]); // Append the character from word1 to the merged string
             }
-            if(i < word2.size()) {
-                ans.push_back(word2[i]);
+            if(i < word2.size()) {       // If there are more characters in word2
+                ans.push_back(word2[i]); // Append the character from word2 to the merged string
             }
         }
-        return ans;
+        return ans; 
     }
 };
