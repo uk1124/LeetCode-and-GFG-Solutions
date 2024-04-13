a87faf822c34ff77cdf989b86048f3bb39c3b9a2
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-13 11:36:50 +0530

    Time: 2 ms (50.18%), Space: 7.8 MB (19%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string ans = "";
-        int count = 0;
+        int maxLength = max(word1.size(), word2.size()); 
 
-        for(int i=0; i<word1.size(); i++) {
-            if(word2[i] == '\0') break;
-            ans.push_back(word1[i]);
-            ans.push_back(word2[i]);
-            count++;
-        }
-
-        if(count < word2.size()) {
-            for(int i=count; i<word2.size(); i++) {
-                ans.push_back(word2[i]);
-            }
-        }
-        else {
-            for(int i=word2.size(); i<word1.size(); i++) {
+        for(int i = 0; i < maxLength; i++) {
+            if(i < word1.size()) {
                 ans.push_back(word1[i]);
+            }
+            if(i < word2.size()) {
+                ans.push_back(word2[i]);
             }
         }
         return ans;
