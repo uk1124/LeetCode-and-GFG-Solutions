c3400d7d229a85f41759806c128e2274f92a72cb
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-16 11:29:09 +0530

    Time: 10 ms (29.55%), Space: 9.9 MB (18.11%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,16 +2,36 @@ class Solution {
 public:
     string reverseVowels(string s) {
         unordered_set<char> vwls = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        string vowels = "";
-        for(char ch: s) {
-            if(vwls.find(ch) != vwls.end()) {
-                vowels += ch;
+        // string vowels = "";
+        // for(char ch: s) {
+        //     if(vwls.find(ch) != vwls.end()) {
+        //         vowels += ch;
+        //     }
+        // }
+        // int j = vowels.size()-1;
+        // for(int i=0; i<s.size(); i++) {
+        //     if(vwls.find(s[i]) != vwls.end()) {
+        //         s[i] = vowels[j--];
+        //     }
+        // }
+        // return s;
+
+        int i = 0, j = s.size()-1;
+        while(i < j) {
+            if(vwls.find(s[i]) != vwls.end() and vwls.find(s[j]) == vwls.end()) {
+                j--;
             }
-        }
-        int j = vowels.size()-1;
-        for(int i=0; i<s.size(); i++) {
-            if(vwls.find(s[i]) != vwls.end()) {
-                s[i] = vowels[j--];
+            else if(vwls.find(s[i]) == vwls.end() and vwls.find(s[j]) != vwls.end()) {
+                i++;
+            }
+            else if(vwls.find(s[i]) == vwls.end() and vwls.find(s[j]) == vwls.end()) {
+                i++;
+                j--;
+            }
+            else {
+                swap(s[i], s[j]);
+                i++;
+                j--;
             }
         }
         return s;
