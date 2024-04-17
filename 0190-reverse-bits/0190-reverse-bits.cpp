5c156569a8cafc7cd1eae3da91b4f847e4d28e30
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-17 12:17:57 +0530

    Time: 0 ms (100%), Space: 7.1 MB (28.96%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t ans = 0;
-        for(int i=0; i<32; i++) {
-            ans = (ans<<1) + (n>>i &1);
+        uint32_t ans = 0; // Initialize the variable to store the reversed bits
+
+        // Iterate through each bit of the input no (32 bits in total)
+        for(int i = 0; i < 32; i++) {
+            // Shift the curr result to the left by 1 position and add the rightmost bit of the input no
+            ans = (ans << 1) + (n >> i & 1);
         }
-        return ans;
+        return ans; 
     }
 };
