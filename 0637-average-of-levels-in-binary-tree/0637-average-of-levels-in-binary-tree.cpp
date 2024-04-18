240292b18b736e6c59dd55817554470088150633
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-18 08:35:33 +0530

    Time: 10 ms (69.78%), Space: 22.2 MB (92.39%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -13,34 +13,33 @@ class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
         if (!root)
-            return {};  // Return an empty vector if the root is null.
+            return {};  // Return an empty vector if the root is null
 
         vector<double> ans;
         queue<TreeNode*> q;
         q.push(root);
 
         while(!q.empty()) {
-            int levelSize = q.size();  // Number of nodes at the current level.
+            int levelSize = q.size();  // Number of nodes at the current level
             double levelSum = 0;
 
             for(int i = 0; i < levelSize; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-
                 levelSum += node->val;
 
                 if(node->left) {
-                    q.push(node->left);
+                    q.push(node->left);   // Enqueue left child if it exists.
                 }
                 if(node->right) {
-                    q.push(node->right);
+                    q.push(node->right);  // Enqueue right child if it exists.
                 }
             }
             
-            double levelAvg = levelSum / levelSize;
-            ans.push_back(levelAvg);
+            double levelAvg = levelSum / levelSize;  // Calculate the avg for the curr level.
+            ans.push_back(levelAvg);  // Push the avg value of the curr level into the result vector
         }
 
-        return ans;
+        return ans;  
     }
 };
