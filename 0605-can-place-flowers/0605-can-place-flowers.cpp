fe1d3ef81999abb2d9cd3393f5a9383e180460b0
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-22 10:42:55 +0530

    Time: 12 ms (52.6%), Space: 22.7 MB (88.38%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        for(int i=0; i<flowerbed.size(); i++) {
-            if(flowerbed[i] == 1) {
-                i += 1;
-                continue;
+        int count = 0;  // A count variable to keep track of the no of flowers that can be planted
+        int size = flowerbed.size();
+        int i = 0;
+        
+        while(i < size) {
+            // Check if the current plot and its adjacent plots are empty
+            if (flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == size-1 or flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1; // Plant a flower in the current plot
+                count++;          // Increment the count of planted flowers
             }
-            n -= 1;
-            i += 1;
+            i++; 
         }
-        return n == 0;
+        
+        // Check if the number of planted flowers is greater than or equal to n
+        return count >= n;
     }
 };
