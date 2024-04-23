d23c74636f683d7619167a3ead41d8599cb15c0e
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-23 11:35:22 +0530

    Time: 5 ms (31.85%), Space: 5.9 MB (100%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int hammingWeight(int n) {
-        string binString = "";
-
-        while(n) {
-            int rem = n % 2;
-            binString += to_string(rem);
-            n /= 2;
+    int hammingWeight(uint32_t n) {
+       int cnt = 0;
+        while(n){
+            cnt++;
+            n &= n-1;
         }
-        reverse(binString.begin(), binString.end());
-        int ans = 0;
-        for(char ch: binString) {
-            if(ch == '1') ans++;
-        }
-        return ans;
+        return cnt;;
     }
 };
