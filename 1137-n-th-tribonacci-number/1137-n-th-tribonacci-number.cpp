15ee87c1e37dda9889ce504543d84a33c4efea50
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-24 08:45:00 +0530

    Time: 3 ms (22.77%), Space: 6 MB (100%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,9 +4,14 @@ public:
         if(n < 2) {
             return n;
         }
-        else if(n == 2) {
-            return 1;
+        int dp[n+1];
+        dp[0] = 0;
+        dp[1] =  dp[2] = 1;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
-        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+
+        return dp[n];
     }
 };
