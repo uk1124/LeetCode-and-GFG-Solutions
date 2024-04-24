9a4e5f662a972c8d83c294db35aa6c07e94f8079
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-24 10:11:04 +0530

    Time: 0 ms (100%), Space: 10 MB (5%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -4,11 +4,12 @@ public:
         int n = gain.size();
         vector<int> prefixSum(n+1);
         prefixSum[0] = 0;
+        int max_alt = 0;
 
         for(int i=1; i<n+1; i++) {
             prefixSum[i] = prefixSum[i-1] + gain[i-1];
+            max_alt = max(max_alt, prefixSum[i]);
         }
-
-        return *max_element(prefixSum.begin(), prefixSum.end());
+        return max_alt;
     }
 };
