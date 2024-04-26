6a7bceebad2c6b5ae595f70604b21205f7f313e6
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-26 08:14:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = 0;
+        int curr = 0;
+        for(int el: nums) {
+            curr += el;
+            ans = max(ans, curr);
+            if(curr < 0) {
+                curr = 0;
+            }
+        }
+        return ans;
+    }
+};
