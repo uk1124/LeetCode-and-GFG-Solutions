43f4984acf85a450d8240f483105b064a6041dcd
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-04-30 12:34:36 +0530

    Time: 4 ms (26.51%), Space: 7.1 MB (86.46%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+/*APPROACH: Calculate the number of trailing zeroes in n! by counting the number of factors of 5 in 
+the prime factorization of n!. Since there are fewer factors of 5 than factors of 2 in the prime 
+factorization of n!, we only need to count the factors of 5.
+*/
     int trailingZeroes(int n) {
         int x = 5;
         int ans = 0;
 
+        // Continue loop until no more factors of 5 in n!
         while(n/x) {
             ans += (n/x);
-            x *= 5;
+            x *= 5; // Update x to the next power of 5
         }
         return ans;
     }
