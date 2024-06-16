cddb4f252669677695a67a28a664310e40087f53
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-06-16 11:30:16 +0530

    Time: 111 ms (80.82%), Space: 68.9 MB (92.19%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
+        int moves = 0;
         sort(nums.begin(), nums.end());
 
-        int i = 0;
-        while(i < n) {
-            if(i+1 < n and nums[i] == nums[i+1]) {
-                ans++;
-                nums[i+1] += 1;
-                sort(nums.begin(), nums.end());
-                i -= 1;
+        for (int i = 1; i < n; i++) {
+            if(nums[i] <= nums[i-1]) {
+                moves += (nums[i-1] + 1 - nums[i]);
+                nums[i] = nums[i-1] + 1;
             }
-            i++;
         }
 
-        return ans;
+        return moves;
     }
 };
