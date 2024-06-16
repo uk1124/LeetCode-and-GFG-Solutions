a04cb1e7fa128909ca8b67e2afc5c44a97ba7a96
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-06-16 22:18:38 +0530

    Time: 19 ms (16.75%), Space: 11.6 MB (99.98%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,41 +11,27 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (!head || !head->next || k == 0) {
+        if(!head) {
             return head;
         }
-
-        // Compute the size of the linked list
-        int size = 0;
-        ListNode* temp = head;
-        while (temp) {
-            size++;
-            temp = temp->next;
+        
+        int len = 1;    //No. of nodes
+        ListNode *newHead, *tail;
+        newHead = tail = head;
+        
+        while(tail->next) {     //Get the no. of nodes in the list
+            tail = tail->next;
+            len++;
         }
+        tail->next = head;      //Circle the link
 
-        // Compute the effective rotations needed
-        k = k % size;
-        if (k == 0) {
-            return head;
+        if(k %= len)  {
+            for(auto i=0; i<len-k; i++) { //The tail node is the (len-k)-th node (first node is head)
+                tail = tail->next;
+            }
         }
-
-        // Find the new tail: (size - k - 1)th node and the new head: (size - k)th node
-        ListNode* newTail = head;
-        for (int i = 0; i < size - k - 1; i++) {
-            newTail = newTail->next;
-        }
-        ListNode* newHead = newTail->next;
-
-        // Break the link to form the new list
-        newTail->next = nullptr;
-
-        // Find the old tail and connect it to the old head
-        ListNode* oldTail = newHead;
-        while (oldTail->next) {
-            oldTail = oldTail->next;
-        }
-        oldTail->next = head;
-
+        newHead = tail->next; 
+        tail->next = NULL;
         return newHead;
     }
 };
