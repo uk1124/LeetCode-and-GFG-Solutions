4c7636ceb3622599e280be88287c5fef2b4b2caf
Utkarsh Shukla <55598047+uk1124@users.noreply.github.com>
2024-07-09 15:17:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        
+/*1 -> 2 .. 1, 2,3
+2-> 5, 4,5,6,7,8
+3-> 4, 9,10,11
+*/
+    int ans = 0;
+    int current_time = 0;
+
+    for(auto v: customers) {
+        int arrival_time = v[0];
+        int cooking_time = v[1];
+
+        if(arrival_time > current_time) {
+            current_time = arrival_time;
+        }
+
+        int waiting_time = current_time - arrival_time + cooking_time;
+        ans += waiting_time;
+
+        current_time += cooking_time;
+    }
+
+    return (double)ans/customers.size();
+
+    }
+};
